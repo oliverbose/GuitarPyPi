de4432e24de2d6be43389167da312502073cdaa9
Oliver Bose <oliver.bose@gmail.com>
2013-03-07 21:13:16 +0000

    Fixes #3. Complete rewrite of button/trigger evaluation. Multiple chords are possible now. If the timing between button and trigger is not ideal, the chord is changed anyway.

--- src/GuitarPyPi.py
+++ src/GuitarPyPi.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
-import random, sys, time, math, pygame
+import pygame
 from pygame.locals import *
 
-numChordButtons = 5
+NO_BUTTON_CHORD = 0
 
 def main():
-    global guitar, chordsOpen, chordsMuted, chordToPlay
+    global guitar, chordsOpen, chordsMuted, chordsToPlay, playing, playOpen
 
     print "Initialization start..."
 
@@ -15,11 +15,13 @@ def main():
     pygame.mixer.pre_init(22050,-16,1,1)
     pygame.init()
     pygame.joystick.init()
+    # we do not need this event which is thrown permanently by the guitar
     pygame.event.set_blocked(JOYAXISMOTION)
 
     guitar = pygame.joystick.Joystick(0)
     guitar.init()
 
+    # map the sounds to the buttons. To be moved to config.
     chordsOpen = { 0: pygame.mixer.Sound("../audio/E-open.ogg"),\
                  1: pygame.mixer.Sound("../audio/G-open.ogg"),\
                  2: pygame.mixer.Sound("../audio/A-open.ogg"),\
@@ -34,49 +36,130 @@ def main():
                  4: pygame.mixer.Sound("../audio/D-muted.ogg"),\
                  5: pygame.mixer.Sound("../audio/E8-muted.ogg")}
                  
-    chordToPlay = 0
+    chordsToPlay = [NO_BUTTON_CHORD]
+    playOpen = True
+    playing = False
+    
     print "Initialization complete"
+
     while True:
         fist()
 
-
+# The main loop. Listens to the eventqueue
 def fist():
     global guitar, chordsOpen, chordsMuted
     
     # this keeps the application idle until an event is caught
-    event = pygame.event.wait()
+    firstevent= pygame.event.wait()
+    # put back the event taken out by wait()
+    pygame.event.post(firstevent)
     #print "GotEvent  " + pygame.event.event_name(event.type)
-    if event.type == JOYHATMOTION:
-        if event.value == (0,0):
-            stopAll()
-        elif event.value == (0,-1):
-            playOpen()
-        elif event.value == (0,1):
-            playMuted()
+    for event in pygame.event.get():
+        if event.type == JOYHATMOTION:
+            handleTrigger(event)
+        elif event.type == JOYBUTTONDOWN:
+            handleButtonDown(event)
+        elif event.type == JOYBUTTONUP:
+            handleButtonUp(event)
 
+
+# Handler if the trigger status has changed
+# (0,0): Trigger released: stop all chords
+# (0,-1): Trigger pushed down: play all chords in the activce chordlist from open chords map
+# (0,1): Trigger pulled up: play all chords in the activce chordlist from muted chords map
+def handleTrigger(event):
+    if event.value == (0,0):
+        stopAll()
+    elif event.value == (0,-1):
+        playOpenChords()
+    elif event.value == (0,1):
+        playMutedChords()
+
+# Handler if a button has been pushed.
+# Adds the selected chord to the list of actice chords.
+# Removes the cord played when no button is pressed from the list and stops it.
+# Immediately plays the chord when the trigger is active
+def handleButtonDown(event):
+    global chordsToPlay
+    chord = getChord(event.button)
+    chordsToPlay.append(chord)
+    if (NO_BUTTON_CHORD in chordsToPlay):
+        chordsToPlay.remove(NO_BUTTON_CHORD)
+        stopChord(NO_BUTTON_CHORD)
+    playChord(chord)
+
+# Handler if a button has been released
+# Removes the selected chord to the list of actice chords
+# Adds the cord played when no button is pressed to the list and plays it if the trigger is active.
+# Immediately stops the chord when the trigger is active.
+def handleButtonUp(event):
+    global chordsToPlay
+    chord = getChord(event.button)
+    chordsToPlay.remove(chord)
+    if (noButtonPresed()):
+        chordsToPlay.append(NO_BUTTON_CHORD)
+        playChord(NO_BUTTON_CHORD)
+    stopChord(chord)
+
+#stops all chords    
 def stopAll():
+    global playing
     pygame.mixer.stop()
+    playing = False
 
-def playOpen():
-    global chordsOpen
-    chordsOpen[getActiveChordButton()].play()
+# stops a single chord    
+def stopChord(chord):
+    chordsOpen[chord].stop()
+    chordsMuted[chord].stop()
 
-def playMuted():
-    global chordsMuted
-    chordsMuted[getActiveChordButton()].play()
+# plays a single chord when the trigger is active 
+def playChord(chord):
+    global playOpen, playing, chordsMuted, chordsOpen
+
+    # as soon as anyother chord than the NO_BUTTON_CHORD is played stop it, otherwise it may be heard chortly when changing a chord
+    if (chord > NO_BUTTON_CHORD):
+        stopChord(NO_BUTTON_CHORD)
+        
+    if (playing and playOpen):
+        chordsOpen[chord].play()
+    if (playing and not playOpen):
+        chordsMuted[chord].play()
     
-def getActiveChordButton():
+# plays chords from the active chords list using the open chords samples
+def playOpenChords():
+    global playing, playOpen, chordsToPlay
+    playing = True
+    playOpen = True
+    for chord in chordsToPlay:
+        playChord(chord)
+
+
+# plays chords from the active chords list using the muted chords samples
+def playMutedChords():
+    global playing, playOpen, chordsToPlay
+    playOpen = False
+    playing = True
+    for chord in chordsToPlay:
+        playChord(chord)
+    playing = True
+
+# Helper that equalizes the turner between button 2 and 3
+def getChord(buttonId):
+    if (buttonId == 2):
+        return buttonId + 2
+    if (buttonId == 3):
+        return buttonId
+    return buttonId + 1
+
+# Helper that 
+def noButtonPresed():
     global guitar
-    buttonId = 0
-    while buttonId < numChordButtons:
-        if (guitar.get_button(buttonId)):
-            if (buttonId == 2):
-                return buttonId + 2
-            if (buttonId == 3):
-                return buttonId
-            return buttonId + 1
-        buttonId = buttonId + 1
-    return 0
+    status =  ( not guitar.get_button(0)\
+            and not guitar.get_button(1)\
+            and not guitar.get_button(2)\
+            and not guitar.get_button(3)\
+            and not guitar.get_button(4))
+    return status
 
 if __name__ == '__main__':
     main()
